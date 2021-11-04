a4777272028496d5db39962d040b63ef50a0e078
Aufa Aqila <93331691+aupax@users.noreply.github.com>
2021-11-04 16:32:28 +0700

    Add files via upload

---
+++ files/password.py
@@ -0,0 +1,73 @@
+import os, sys
+
+print ("\033[1;32mLogin system by aupaxyz")
+
+print ("\033[1;32m-----------------------")
+
+print ("\033[1;32mSilahkan masukkan username dan password anda")
+
+print ("\033[1;32m                       ")
+
+username = 'aupaxyz'
+
+password = 'aupa'
+
+
+
+def restart():
+  
+  ngulang = sys.executable
+  
+  os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+  
+  uname = raw_input("username : ")
+
+  if uname == username:
+  
+    pwd = raw_input("password : ")
+  
+  
+  
+    if pwd == password:
+ 
+      print ("\033[1;32m               ")
+
+      print "\033[1;32mBerhasil login..",
+
+      sys.exit()
+
+
+
+    else:
+
+      print "\033[1;32mMaaf password anda salah! \033[00m"
+
+      print "Segera login ulang..\n"
+
+      restart()
+ 
+ 
+ 
+  else:
+
+    print "Maaf username anda salah! \033[00m"
+
+    print "Segera login ulang..\n"
+
+    restart()
+
+
+
+try:
+
+  main()
+
+except KeyboardInterrupt:
+
+  os.system('clear')
+
+  restart()
---
+++ files/start.sh
@@ -0,0 +1,47 @@
+clear
+bl='\033[34;1m' #blue
+gr='\033[32;1m' #green
+pu='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+re='\033[31;1m' #red
+ma='\033[37;1m' #magenta
+ye='\033[33;1m' #yellow
+echo
+echo
+echo "Preparing Packages..."
+echo
+echo
+sleep 2
+clear
+echo
+echo
+echo "Installing Python2..."
+echo
+echo
+pkg install python2
+echo
+echo "Python2 Succesfully Installed"
+echo
+sleep 2
+clear
+echo
+echo
+echo "Opening password.py..."
+echo
+echo
+sleep 2
+python2 password.py
+echo
+echo
+sleep 2
+clear
+echo
+# Your Direct
+echo
+echo "Loading xoxo.py..."
+echo
+sleep 2
+clear
+python xoxo.py
+# Your Direct
+echo
