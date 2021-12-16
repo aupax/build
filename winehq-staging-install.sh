01aa378ddd94428539e5b2b61bdae02ea8fc05ab
Aufa Aqila <93331691+aupax@users.noreply.github.com>
2021-12-16 16:01:33 +0700

    Create winehq-staging-install.sh

---
+++ winehq-staging-install.sh
@@ -0,0 +1,9 @@
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+sudo apt update
+sudo apt install --install-recommends winehq-staging
+wine --version
+winecfg
+#
