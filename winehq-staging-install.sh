f8cee17901ab38c65b5b60a6029b25e424a2c957
Aufa Aqila <93331691+aupax@users.noreply.github.com>
2021-12-16 16:06:01 +0700

    Update winehq-staging-install.sh

--- winehq-staging-install.sh
+++ winehq-staging-install.sh
@@ -1,9 +1,23 @@
+# change linux to 32bit
 sudo dpkg --add-architecture i386
+# import winehq.key
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
+# add winehq.key to directory
 sudo apt-key add winehq.key
+# add winehq repository for debian 18.04 LTS
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+# update packages and repository
 sudo apt update
+# install winehq-staging
 sudo apt install --install-recommends winehq-staging
+# check installed wine / version of wine
 wine --version
+# configure winehq
 winecfg
-#
+
+# RUN .EXE FILES
+# wine namefiles.exe
+# or you can just click .exe files like on windows
+
+# ©AUPAXYZ 2021
+
